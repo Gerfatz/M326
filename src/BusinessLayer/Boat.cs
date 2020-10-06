b505258393642c4659e5d76a4f066d2195b97d6c
Benjamin Zoronjic <benjamin.zoronjic@expersoft.com>
2020-10-06 10:21:13 +0200

    Added  private CreateBoatBits function

--- src/BusinessLayer/Boat.cs
+++ src/BusinessLayer/Boat.cs
@@ -40,53 +40,55 @@ namespace BusinessLayer
         /// <returns>Returns the created boat</returns>
         public Boat CreateBoat(Position startPos, Position endPos)
         {
-            byte start;
-            byte end;
-
             if (startPos.X == endPos.X)
             {
-                if (startPos.Y > endPos.Y)
-                {
-                    start = endPos.Y;
-                    end = startPos.Y;
-                }
-                else
-                {
-                    start = startPos.Y;
-                    end = endPos.Y;
-                }
-
-                CreateBoatBits(start, end, endPos.X);
+                CreateBoatBits(startPos.Y, endPos.Y, endPos.X, true);
             }
             else if (startPos.Y == endPos.Y)
             {
-                if (startPos.Y > endPos.Y)
-                {
-                    start = endPos.Y;
-                    end = startPos.Y;
-                }
-                else
-                {
-                    start = startPos.Y;
-                    end = endPos.Y;
-                }
-                CreateBoatBits(start, end, endPos.Y);
+                CreateBoatBits(startPos.X, endPos.X, endPos.Y, false);
+            }
+            else
+            {
+                return null;
             }
 
 
             return this;
         }
 
-        private void CreateBoatBits(byte start, byte end, byte axis)
+        private void CreateBoatBits(byte startCord, byte endCord, byte staticAxis, bool isXAxis)
         {
             byte difference;
+            byte start;
+            byte end;
+
+            if (startCord > endCord)
+            {
+                start = startCord;
+                end = endCord;
+            }
+            else
+            {
+                start = endCord;
+                end = startCord;
+            }
+
 
             difference = (byte)(start - end);
 
             for (byte i = 0; i <= difference; i++)
             {
-                Position pos = new Position(start, (byte)(axis + i));
-                BoatBits.Add(new BoatBit(pos));
+                if (isXAxis)
+                {
+                    Position pos = new Position(staticAxis, (byte)(end + i));
+                    this.BoatBits.Add(new BoatBit(pos));
+                }
+                else
+                {
+                    Position pos = new Position((byte)(end + i), staticAxis);
+                    this.BoatBits.Add(new BoatBit(pos));
+                }
             }
         }
     }
