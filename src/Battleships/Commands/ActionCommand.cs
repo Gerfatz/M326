1e30d6280b7e89c668445df3b06f70783ca90cc5
Dario Camenzind <dario.camenzind@gmail.com>
2020-12-02 23:01:09 +0100

    Started adding real mvvm

---
+++ src/Battleships/Commands/ActionCommand.cs
@@ -0,0 +1,50 @@
+﻿using BusinessLayer;
+using BusinessLayer.Storage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Battleships.Commands
+{
+    public class ActionCommand : ICommand
+    {
+        private readonly Action _action;
+        private readonly Func<bool> _canExecuteAction;
+
+        public ActionCommand(Action action) : this(action, () => true)
+        {
+        }
+
+        public ActionCommand(Action action, Func<bool> canExecute)
+        {
+            _action = action;
+            _canExecuteAction = canExecute;
+        }
+
+        public Guid Guid { get; set; }
+        public Field Field { get; set; }
+
+        public void Execute(object obj)
+        {
+            _action();
+        }
+
+        public bool CanExecute(object obj)
+        {
+            return _canExecuteAction();
+        }
+
+        public event EventHandler CanExecuteChanged 
+        { 
+            add 
+            { 
+                CommandManager.RequerySuggested += value; 
+            } 
+            remove 
+            { 
+                CommandManager.RequerySuggested -= value;
+            } 
+        }
+    }
+}
--- src/Battleships/ViewModels/PlayingFieldViewModel.cs
+++ src/Battleships/ViewModels/PlayingFieldViewModel.cs
@@ -1,22 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
+using Battleships.Commands;
 using BusinessLayer;
+using BusinessLayer.Storage;
 
 namespace Battleships.ViewModels
 {
-    public class PlayingFieldViewModel
+    public class PlayingFieldViewModel : INotifyPropertyChanged
     {
+        private readonly Fieldsaver _fieldsaver;
+        private Dictionary<Guid, string> _fields;
+        private Field _selectedField;
+        private Guid _selectedFieldGuid;
+
         // Properties
         public bool? DeleteState { get; set; } = false; // State of "delete" checkbox
-        public Field Field { get; set; } // Created field from business layer
+        public Field SelectedField {
+            get => _selectedField;
+            private set
+            {
+                _selectedField = value;
+                OnPropertyChanged();
+            }
+        } // Created field from business layer
+
         public bool EditorMode { get; set; } = true; // State if editor mode is on
         public List<Position> SelectedPositions { get; set; } // List of positions selected by the user
+
+        public Dictionary<Guid, string> Fields => _fields;
+
+        //Commands
+        public ActionCommand SaveCommand { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         // Constructor
         public PlayingFieldViewModel()
         {
             SelectedPositions = new List<Position>();
+            SaveCommand = new ActionCommand(Save);
+            _fieldsaver = new Fieldsaver();
+            _fields = _fieldsaver.GetAllFields();
+        }
+
+        public void Save()
+        {
+            _fieldsaver.Save(_selectedField, _selectedFieldGuid);
+        }
+
+        public void Load()
+        {
+            SelectedField = _fieldsaver.GetField(_selectedFieldGuid);
+        }
+
+        private void OnPropertyChanged([CallerMemberName]string property = null)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(property));
         }
     }
 }
