c035a190608a7ce9cc062e5295d2d2dcf0b29d9d
PersonCopy <bzoronjic2@gmail.com>
2020-12-15 20:35:52 +0100

    Added UnitTests of "Boat" class
    "Boat" class unit tests added, as well as a Validation test in
    "FieldTests.cs"

--- src/Tests/FieldTests.cs
+++ src/Tests/FieldTests.cs
@@ -28,7 +28,7 @@ namespace Tests
 
             List<BoatBit> boatBits = boat.BoatBits;
 
-            bool isNotDiagonal = (boatBits[0].XYPosition.X == boatBits[boatBits.Count - 1].XYPosition.X ||
+            bool isNotDiagonal = (boatBits[0].XYPosition.X == boatBits[^1].XYPosition.X ||
                 boatBits[0].XYPosition.Y == boatBits[^1].XYPosition.Y) ;
 
             Assert.IsTrue(isNotDiagonal);
@@ -55,5 +55,35 @@ namespace Tests
 
             Assert.IsFalse(field.Boats.Any(x => field.ValidateBoatPos(x)));
         }
+
+        [Test]
+        public void TestValidation()
+        {
+            Field field = new Field(7);
+
+            field.GenerateBoats();
+
+            field.CreateBoat(new Position(5, 2), new Position(3, 2));
+
+            Assert.IsFalse(field.CreateBoat(new Position(5, 2), new Position(5, 4)));
+            //Assert.IsTrue(field.CreateBoat(new Position(1, 1), new Position(1, 1)));
+        }
+
+        /*
+        [Test]
+        public void TestValidationValid()
+        {
+            Field validationField = new Field(7);
+
+            validationField.GenerateBoats();
+
+            validationField.CreateBoat(new Position(6, 6), new Position(6, 6));
+
+            bool validation = validationField.CreateBoat(new Position(1, 1), new Position(1, 1));
+
+            
+
+            Assert.AreEqual(2, validationField.Boats.Count);
+        }*/
     }
 }
